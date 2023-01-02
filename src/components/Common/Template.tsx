9b4dcb972f7f82603382c30da9eaaa0da213f702
Serzhul <wilshere917@gmail.com>
2023-01-02 10:56:57 +0900

    Fix : Build Error fix

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -33,6 +33,10 @@ const Template: FunctionComponent<TemplateProps> = function ({
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
 
         <meta property="og:type" content="website" />
         <meta property="og:title" content={title} />
